9e3090d54f88d842d4307e9a5f2934748228f935
alkxsd <aleks.masayahin@gmail.com>
2024-10-04 17:30:02 +0800

    Changes on login screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,16 +45,20 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+            <div class="flex items-center justify-between mt-4">
+                <x-button outline :href="route('register')" label="Register" blue />
 
-                <x-button type="submit" class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
+                <div>
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+
+                    <x-button type="submit" class="ms-4" blue>
+                        {{ __('Log in') }}
+                    </x-button>
+                </div>
             </div>
         </form>
     </x-authentication-card>
