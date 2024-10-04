1338dadd2c4b6a69a81c84cfcf4d7521756de50b
alkxsd <aleks.masayahin@gmail.com>
2024-10-04 16:01:04 +0800

    Implementing Policy to check for user-tasks specific authorization

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -9,9 +9,12 @@ use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Controllers\Controller;
 use App\DTO\TaskDto;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class TaskController extends Controller
 {
+    use AuthorizesRequests;
+
     protected $taskService;
 
     public function __construct(TaskService $taskService)
@@ -34,11 +37,13 @@ class TaskController extends Controller
 
     public function show(Task $task)
     {
+        $this->authorize('view', $task);
         return response()->json($task);
 }
 
     public function update(UpdateTaskRequest $request, Task $task)
     {
+        $this->authorize('update', $task);
         $taskDto = TaskDto::fromRequest($request);
         $task = $this->taskService->updateTask($task, $taskDto);
         return response()->json($task);
@@ -46,12 +51,14 @@ class TaskController extends Controller
 
     public function destroy(Task $task)
     {
+        $this->authorize('delete', $task);
         $this->taskService->deleteTask($task);
         return response()->noContent();
     }
 
     public function updateStatus(Request $request, Task $task)
     {
+        $this->authorize('update', $task);
         $request->validate(['status' => 'required|string']);
         $task = $this->taskService->updateTaskStatus($task, $request->status);
         return response()->json($task);
